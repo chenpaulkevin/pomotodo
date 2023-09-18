5e4cf888a468a6e372baf7de2d9f10a9989fd622
chenpaulkevin <chenpaulkevin@gmail.comssh-keygen -t rsa -b 4096 -C chenpaulkevin@gmail.com>
2023-09-18 16:23:56 +0800

    Navbar design update

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -4,14 +4,16 @@ import Link from 'next/link'
 
 export default function Navbar() {
   return (
-    <nav className="mx-auto max-w-container px-4 sm:px-6 lg:px-8 bg-secondary text-primary rounded-lg">
-    <div className="relative flex items-center py-[2.125rem]">
-    <h1><Link href="/" className="flex-none text-slate-900 text-3xl font-extrabold">Pomotodo</Link></h1> 
-    <div className="ml-auto hidden lg:flex lg:items-center">
-    <Link href="/" className="ml-8 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Home</Link>
-    <Link href="/leaderboard" className='ml-8 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900'>Leaderboard</Link>
-    </div>
-    </div>
+    <nav className='flex justify-between items-center rounded-lg  px-8 py-6 lg:px-16 md:py-8 border-black border'>
+        <div className="logo"><span className='font-bold text-2xl md:text-3xl lg:text-4xl'>Pomotoro</span></div>
+        <div className="nav-links collapse md:visible">
+          <ul className='flex items-center gap-4'>
+            <Link href="/"><li className='font-semibold text-lg'>Home</li></Link>
+            <Link href="/leaderboard"><li className='font-semibold text-lg'>Leaderboard</li></Link>
+            <Link href="/leaderboard"><li className='font-semibold text-lg'>Profile</li></Link>
+            <Link href="/leaderboard"><li className='font-semibold text-lg bg-black text-white rounded-md px-4 py-1'>Start</li></Link>
+          </ul>
+        </div>
     </nav>
   )
 }

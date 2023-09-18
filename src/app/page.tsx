187ea5657a1d6da861cb9bddba6817e7e7363199
chenpaulkevin <chenpaulkevin@gmail.comssh-keygen -t rsa -b 4096 -C chenpaulkevin@gmail.com>
2023-09-18 21:54:18 +0800

    Remove Spline Test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-"use client";
-import Spline from '@splinetool/react-spline';
+//"use client";
+//import Spline from '@splinetool/react-spline';
+//<Spline scene="https://prod.spline.design/DNV71tby5tu-QXdM/scene.splinecode" />
+import React from "react";
 
-export default function App() {
-  return (
-    <div><Spline scene="https://prod.spline.design/DNV71tby5tu-QXdM/scene.splinecode" /></div>
-    
-  );
+export default function Home() {
+  return <div>Home</div>;
 }
-

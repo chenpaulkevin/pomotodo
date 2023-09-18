e99b757d2614a43e7193a43321dd1d215a754b86
chenpaulkevin <chenpaulkevin@gmail.comssh-keygen -t rsa -b 4096 -C chenpaulkevin@gmail.com>
2023-09-18 16:38:19 +0800

    Spline Model Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import Spline from '@splinetool/react-spline';
 
 export default function App() {
   return (
-    <Spline scene="https://prod.spline.design/DNV71tby5tu-QXdM/scene.splinecode" />
+    <div><Spline scene="https://prod.spline.design/DNV71tby5tu-QXdM/scene.splinecode" /></div>
+    
   );
 }
+

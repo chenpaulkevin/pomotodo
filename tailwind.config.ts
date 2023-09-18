7e8f89464640eaa903cb5d595a216b320da5ac74
chenpaulkevin <chenpaulkevin@gmail.comssh-keygen -t rsa -b 4096 -C chenpaulkevin@gmail.com>
2023-09-18 16:22:30 +0800

    Revert tailwind Config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,7 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-        colors: {
-          primary: '#7856ff',
-          secondary: '#ffffff'
-        }
-    },
+
   },
   plugins: [],
 }

95e493b09ca9a73bb97e7b8d4e61e64e7bc55447
chenpaulkevin <chenpaulkevin@gmail.comssh-keygen -t rsa -b 4096 -C chenpaulkevin@gmail.com>
2023-09-19 22:12:55 +0800

    Add skeleton for mobile side navigation

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -2,48 +2,87 @@
 import React, { useState } from "react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import Link from "next/link";
+import { motion } from "framer-motion";
+
+const navLinks = [
+  { title: "Home", href: "/" },
+  { title: "Leaderboard", href: "/leaderboard" },
+  { title: "Profile", href: "/profile" },
+  { title: "Work", href: "/timer" },
+];
 
 export default function navbar() {
   const [open, setOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
   return (
-    <nav>
-      <div className="flex justify-between items-center rounded-lg  px-8 py-6 lg:px-16 md:py-8 border-black border-2">
-        <div className="logo">
-          <span className="font-display text-xl md:text-2xl lg:text-3xl">
-            Pomotoro
-          </span>
+    <header>
+      <nav>
+        <div className="flex justify-between items-center rounded-lg  px-8 py-6 lg:px-16 md:py-8 border-black border-2">
+          <div className="logo">
+            <span className="font-display text-xl md:text-2xl lg:text-3xl">
+              Pomotoro
+            </span>
+          </div>
+          <div className="justify-self-end lg:hidden">
+            <button
+              className="transition-all duration-200"
+              onClick={() => setOpen(!open)}
+            >
+              {open ? (
+                <XMarkIcon className="h-8 w-8 stroke-thick"></XMarkIcon>
+              ) : (
+                <Bars3Icon className="h-8 w-8 stroke-thick"></Bars3Icon>
+              )}
+            </button>
+          </div>
         </div>
-        <div className="nav-links hidden lg:flex">
-          <ul className="flex items-center gap-4">
-            <Link href="/">
-              <li className="font-semibold text-lg">Home</li>
-            </Link>
-            <Link href="/leaderboard">
-              <li className="font-semibold text-lg">Leaderboard</li>
-            </Link>
-            <Link href="/profile">
-              <li className="font-semibold text-lg">Profile</li>
-            </Link>
-            <Link href="/timer">
-              <li className="font-bold shadow-solid-primary border border-2 border-black text-lg bg-white text-black rounded-md px-4 py-1">
-                Start
-              </li>
-            </Link>
-          </ul>
+      </nav>
+
+      {open && (
+        <div className="fixed left-0 top-0 w-full h-screen bg-yellow-400 text-black p-10 lg:hidden">
+          <div className="flex h-full flex-col">
+            <div className="flex justify-between">
+              <span className="font-display text-xl md:text-2xl lg:text-3xl">
+                Pomotoro
+              </span>
+              <button
+                className="cursor-pointer text-md text-black"
+                onClick={toggleMenu}
+              >
+                <XMarkIcon className="h-8 w-8 stroke-thick"></XMarkIcon>
+              </button>
+            </div>
+            <div className="flex flex-col h-full justify-center items-center gap-4">
+              {navLinks.map((link, index) => {
+                return (
+                  <MobileNavLink
+                    key={index}
+                    title={link.title}
+                    href={link.href}
+                  />
+                );
+              })}
+            </div>
+          </div>
         </div>
-        <div className="justify-self-end lg:hidden">
-          <button
-            className="transition-all duration-200"
-            onClick={() => setOpen(!open)}
-          >
-            {open ? (
-              <XMarkIcon className="h-8 w-8 stroke-thick"></XMarkIcon>
-            ) : (
-              <Bars3Icon className="h-8 w-8 stroke-thick"></Bars3Icon>
-            )}
-          </button>
-        </div>
-      </div>
-    </nav>
+      )}
+    </header>
   );
 }
+
+const MobileNavLink = ({ title, href }: { title: string; href: any }) => {
+  return (
+    <div>
+      <Link
+        className="text-extrabold subpixel-antialiased text-4xl md:text-6xl"
+        href={href}
+      >
+        {title}
+      </Link>
+    </div>
+  );
+};

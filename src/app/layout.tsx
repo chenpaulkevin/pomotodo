06a05988c9fde4d37b26187f68c82775b063058d
chenpaulkevin <chenpaulkevin@gmail.comssh-keygen -t rsa -b 4096 -C chenpaulkevin@gmail.com>
2023-09-19 17:38:32 +0800

    Set Nunito as global font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,14 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
+import { Nunito } from "next/font/google";
 
 //Components:
 import Navbar from "./components/navbar";
 
 //Default Font:
-const poppins = Poppins({
+const poppins = Nunito({
   subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata: Metadata = {

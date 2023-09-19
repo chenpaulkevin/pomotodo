f4e5f01bbb7ed0c04ecca5a3a6b39f746bc7d195
chenpaulkevin <chenpaulkevin@gmail.comssh-keygen -t rsa -b 4096 -C chenpaulkevin@gmail.com>
2023-09-19 22:51:19 +0800

    Add mobile menu open/close animation

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import Link from "next/link";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 const navLinks = [
   { title: "Home", href: "/" },
@@ -10,6 +10,26 @@ const navLinks = [
   { title: "Profile", href: "/profile" },
   { title: "Work", href: "/timer" },
 ];
+
+const menuVariants = {
+  initial: {
+    scaleY: 0,
+  },
+  animate: {
+    scaleY: 1,
+    transition: {
+      duration: 0.5,
+      ease: [0.12, 0, 0.39, 0],
+    },
+  },
+  exit: {
+    scaleY: 0,
+    transition: {
+      duration: 0.5,
+      ease: [0.12, 0, 0.39, 1],
+    },
+  },
+};
 
 export default function navbar() {
   const [open, setOpen] = useState(false);
@@ -39,37 +59,58 @@ export default function navbar() {
               )}
             </button>
           </div>
-        </div>
-      </nav>
-
-      {open && (
-        <div className="fixed left-0 top-0 w-full h-screen bg-yellow-400 text-black p-10 lg:hidden">
-          <div className="flex h-full flex-col">
-            <div className="flex justify-between">
-              <span className="font-display text-xl md:text-2xl lg:text-3xl">
-                Pomotoro
-              </span>
-              <button
-                className="cursor-pointer text-md text-black"
-                onClick={toggleMenu}
-              >
-                <XMarkIcon className="h-8 w-8 stroke-thick"></XMarkIcon>
-              </button>
-            </div>
-            <div className="flex flex-col h-full justify-center items-center gap-4">
+          <div className="nav-links hidden lg:block">
+            <ul className="flex items-center gap-4">
               {navLinks.map((link, index) => {
                 return (
-                  <MobileNavLink
+                  <DesktopNavLink
                     key={index}
                     title={link.title}
                     href={link.href}
                   />
                 );
               })}
-            </div>
+            </ul>
           </div>
         </div>
-      )}
+      </nav>
+      {/* Mobile Navigation Links */}
+      <AnimatePresence>
+        {open && (
+          <motion.div
+            variants={menuVariants}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            className="fixed left-0 top-0 w-full h-screen origin-top bg-yellow-400 text-black p-10 lg:hidden"
+          >
+            <div className="flex h-full flex-col">
+              <div className="flex justify-between">
+                <span className="font-display text-xl md:text-2xl lg:text-3xl">
+                  Pomotoro
+                </span>
+                <button
+                  className="cursor-pointer text-md text-black"
+                  onClick={toggleMenu}
+                >
+                  <XMarkIcon className="h-8 w-8 stroke-thick"></XMarkIcon>
+                </button>
+              </div>
+              <div className="flex flex-col h-full justify-center items-center gap-4">
+                {navLinks.map((link, index) => {
+                  return (
+                    <MobileNavLink
+                      key={index}
+                      title={link.title}
+                      href={link.href}
+                    />
+                  );
+                })}
+              </div>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </header>
   );
 }
@@ -85,4 +126,24 @@ const MobileNavLink = ({ title, href }: { title: string; href: any }) => {
       </Link>
     </div>
   );
+};
+
+const DesktopNavLink = ({ title, href }: { title: string; href: any }) => {
+  if (title === "Work") {
+    return (
+      <ul className="shadow-solid-primary px-4 py-1 rounded-md border border-2 border-black cursor-pointer">
+        <Link className="font-black text-lg" href={href}>
+          {title}
+        </Link>
+      </ul>
+    );
+  } else {
+    return (
+      <ul className="cursor-pointer">
+        <Link className="font-extrabold text-lg" href={href}>
+          {title}
+        </Link>
+      </ul>
+    );
+  }
 };

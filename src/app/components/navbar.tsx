781d27c5cd402a45a79d29ba73e5fe6da9bed08c
chenpaulkevin <chenpaulkevin@gmail.comssh-keygen -t rsa -b 4096 -C chenpaulkevin@gmail.com>
2023-09-19 17:38:44 +0800

    Add state for mobile hamburger menu

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,31 +1,48 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import Link from "next/link";
 
-export default function Navbar() {
+export default function navbar() {
+  const [open, setOpen] = useState(false);
   return (
-    <nav className="flex justify-between items-center rounded-lg  px-8 py-6 lg:px-16 md:py-8 border-black border">
-      <div className="logo">
-        <span className="font-bold text-2xl md:text-3xl lg:text-4xl">
-          Pomotoro
-        </span>
-      </div>
-      <div className="nav-links collapse md:visible">
-        <ul className="flex items-center gap-4">
-          <Link href="/">
-            <li className="font-semibold text-lg">Home</li>
-          </Link>
-          <Link href="/leaderboard">
-            <li className="font-semibold text-lg">Leaderboard</li>
-          </Link>
-          <Link href="/profile">
-            <li className="font-semibold text-lg">Profile</li>
-          </Link>
-          <Link href="/timer">
-            <li className="font-semibold text-lg bg-black text-white rounded-md px-4 py-1">
-              Start
-            </li>
-          </Link>
-        </ul>
+    <nav>
+      <div className="flex justify-between items-center rounded-lg  px-8 py-6 lg:px-16 md:py-8 border-black border-2">
+        <div className="logo">
+          <span className="font-display text-xl md:text-2xl lg:text-3xl">
+            Pomotoro
+          </span>
+        </div>
+        <div className="nav-links hidden lg:flex">
+          <ul className="flex items-center gap-4">
+            <Link href="/">
+              <li className="font-semibold text-lg">Home</li>
+            </Link>
+            <Link href="/leaderboard">
+              <li className="font-semibold text-lg">Leaderboard</li>
+            </Link>
+            <Link href="/profile">
+              <li className="font-semibold text-lg">Profile</li>
+            </Link>
+            <Link href="/timer">
+              <li className="font-bold shadow-solid-primary border border-2 border-black text-lg bg-white text-black rounded-md px-4 py-1">
+                Start
+              </li>
+            </Link>
+          </ul>
+        </div>
+        <div className="justify-self-end lg:hidden">
+          <button
+            className="transition-all duration-200"
+            onClick={() => setOpen(!open)}
+          >
+            {open ? (
+              <XMarkIcon className="h-8 w-8 stroke-thick"></XMarkIcon>
+            ) : (
+              <Bars3Icon className="h-8 w-8 stroke-thick"></Bars3Icon>
+            )}
+          </button>
+        </div>
       </div>
     </nav>
   );

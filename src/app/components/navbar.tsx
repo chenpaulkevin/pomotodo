283bc0d988a8f0d306d8d316e512ff6fdfac6761
chenpaulkevin <chenpaulkevin@gmail.comssh-keygen -t rsa -b 4096 -C chenpaulkevin@gmail.com>
2023-09-20 22:52:23 +0800

    Fix Navigation Links

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -81,6 +81,22 @@ const mobileLinkVars = {
   },
 };
 
+const navBarTransition = {
+  initial: {
+    y: "-100%",
+    opacity: 0,
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.45,
+      delay: 0.25,
+      ease: [0.5, 0, 0.65, 1],
+    },
+  },
+};
+
 function Navbar() {
   const [open, setOpen] = useState(false);
 
@@ -101,108 +117,123 @@ function Navbar() {
     );
   };
 
+  const DesktopNavLink = ({ title, href }: { title: string; href: any }) => {
+    if (title === "Work") {
+      return (
+        <ul className="shadow-solid-primary px-4 py-1 rounded-md border border-2 border-black cursor-pointer">
+          <Link className="font-black text-lg" href={href}>
+            {title}
+          </Link>
+        </ul>
+      );
+    } else {
+      return (
+        <ul className="cursor-pointer">
+          <Link className="font-extrabold text-lg" href={href}>
+            {title}
+          </Link>
+        </ul>
+      );
+    }
+  };
+
   return (
     <header>
-      <nav>
-        <div className="flex justify-between items-center rounded-lg  px-8 py-6 lg:px-16 md:py-8 border-black border-2">
-          <div className="logo">
-            <span className="font-display text-xl md:text-2xl lg:text-3xl">
-              Pomotoro
-            </span>
-          </div>
-          <div className="justify-self-end lg:hidden">
-            <button
-              className="transition-all duration-200"
-              onClick={() => setOpen(!open)}
-            >
-              {open ? (
-                <Bars3Icon className="h-8 w-8 stroke-black stroke-thick"></Bars3Icon>
-              ) : (
-                <Bars3Icon className="h-8 w-8 stroke-black stroke-thick"></Bars3Icon>
-              )}
-            </button>
-          </div>
-          <div className="nav-links hidden lg:block">
-            <ul className="flex items-center gap-4">
-              {navLinks.map((link, index) => {
-                return (
-                  <DesktopNavLink
-                    key={index}
-                    title={link.title}
-                    href={link.href}
-                  />
-                );
-              })}
-            </ul>
-          </div>
-        </div>
-      </nav>
-      {/* Mobile Navigation Links */}
       <AnimatePresence>
-        {open && (
-          <motion.div
-            variants={menuVariants}
-            initial="initial"
-            animate="animate"
-            exit="exit"
-            className="fixed left-0 top-0 w-full h-screen origin-top bg-black text-black p-10 lg:hidden"
-          >
-            <div className="flex h-full flex-col m-4">
+        <motion.div
+          variants={navBarTransition}
+          initial="initial"
+          animate="animate"
+        >
+          <nav>
+            <div className="flex justify-between items-center rounded-lg  px-8 py-6 lg:px-16 md:py-8 border-black border-2">
+              <div className="logo">
+                <Link href="/">
+                  <span className="font-display text-xl md:text-2xl lg:text-3xl">
+                    Pomotoro
+                  </span>
+                </Link>
+              </div>
+              <div className="justify-self-end lg:hidden">
+                <button
+                  className="transition-all duration-200"
+                  onClick={() => setOpen(!open)}
+                >
+                  {open ? (
+                    <Bars3Icon className="h-8 w-8 stroke-black stroke-thick"></Bars3Icon>
+                  ) : (
+                    <Bars3Icon className="h-8 w-8 stroke-black stroke-thick"></Bars3Icon>
+                  )}
+                </button>
+              </div>
+              <div className="nav-links hidden lg:block">
+                <ul className="flex items-center gap-4">
+                  {navLinks.map((link, index) => {
+                    return (
+                      <DesktopNavLink
+                        key={index}
+                        title={link.title}
+                        href={link.href}
+                      />
+                    );
+                  })}
+                </ul>
+              </div>
+            </div>
+          </nav>
+          {/* Mobile Navigation Links */}
+          <AnimatePresence>
+            {open && (
               <motion.div
-                variants={headerVariants}
+                variants={menuVariants}
                 initial="initial"
                 animate="animate"
                 exit="exit"
-                className="flex justify-between"
+                className="fixed left-0 top-0 w-full h-screen origin-top bg-black text-black p-10 lg:hidden"
               >
-                <span className="font-display text-white text-xl md:text-2xl lg:text-3xl">
-                  Pomotoro
-                </span>
-                <button className="cursor-pointer text-md" onClick={toggleMenu}>
-                  <XMarkIcon className="h-8 w-8 stroke-thick stroke-white"></XMarkIcon>
-                </button>
+                <div className="flex h-full flex-col mx-3 my-3 ml-4">
+                  <motion.div
+                    variants={headerVariants}
+                    initial="initial"
+                    animate="animate"
+                    exit="exit"
+                    className="flex justify-between"
+                  >
+                    <Link href="/" onClick={toggleMenu}>
+                      <span className="font-display text-white text-xl md:text-2xl lg:text-3xl">
+                        Pomotoro
+                      </span>
+                    </Link>
+                    <button
+                      className="cursor-pointer text-md"
+                      onClick={toggleMenu}
+                    >
+                      <XMarkIcon className="h-8 w-8 stroke-thick stroke-white"></XMarkIcon>
+                    </button>
+                  </motion.div>
+                  <motion.div
+                    variants={containerVars}
+                    initial="initial"
+                    animate="open"
+                    exit="initial"
+                    className="flex flex-col h-full justify-center gap-6"
+                  >
+                    {navLinks.map((link, index) => {
+                      return (
+                        <div className="overflow-hidden" key={index}>
+                          <MobileNavLink title={link.title} href={link.href} />
+                        </div>
+                      );
+                    })}
+                  </motion.div>
+                </div>
               </motion.div>
-              <motion.div
-                variants={containerVars}
-                initial="initial"
-                animate="open"
-                exit="initial"
-                className="flex flex-col h-full justify-center items-center gap-4"
-              >
-                {navLinks.map((link, index) => {
-                  return (
-                    <div className="overflow-hidden" key={index}>
-                      <MobileNavLink title={link.title} href={link.href} />
-                    </div>
-                  );
-                })}
-              </motion.div>
-            </div>
-          </motion.div>
-        )}
+            )}
+          </AnimatePresence>
+        </motion.div>
       </AnimatePresence>
     </header>
   );
 }
-
-const DesktopNavLink = ({ title, href }: { title: string; href: any }) => {
-  if (title === "Work") {
-    return (
-      <ul className="shadow-solid-primary px-4 py-1 rounded-md border border-2 border-black cursor-pointer">
-        <Link className="font-black text-lg" href={href}>
-          {title}
-        </Link>
-      </ul>
-    );
-  } else {
-    return (
-      <ul className="cursor-pointer">
-        <Link className="font-extrabold text-lg" href={href}>
-          {title}
-        </Link>
-      </ul>
-    );
-  }
-};
 
 export default Navbar;
